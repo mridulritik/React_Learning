31a2ae3117e0f62f39fa7c2db6ef423282974d0d
mridulritik <mridultiwari522@gmail.com>
2025-06-19 00:58:36 +0530

    use state hook first practicle

--- reactSeries/src/App.jsx
+++ reactSeries/src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react"
 
 
 function App() {
-
+  let [counter, setCounter] = useState(15)
+  const add = ()=>{
+    counter= counter+1
+    setCounter(counter)
+  }
   return (
-  <h1>hello mridul</h1> 
+    <>
+      <h1>counter count {counter}</h1>
+      <button onClick={add}>increase button {counter}</button>
+    </>
   )
 }
 
